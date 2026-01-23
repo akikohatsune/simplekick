4edc304eb681c966745fda9d1e9eae3377eab4ce
AkikoC <k3212700@gmail.com>
2026-01-23 23:25:11 +0000

    upadet

--- bot.py
+++ bot.py
@@ -66,6 +66,7 @@ PRESENCE_TEXT = os.getenv("PRESENCE_TEXT", "Auto-disconnect self-deafen")
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    
 )
 logger = logging.getLogger("simplekick")
 
